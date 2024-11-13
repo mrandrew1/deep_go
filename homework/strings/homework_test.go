998eb4086abc70529c0fd20f0113039b9ef7d1c9
Гусев Андрей <gusev.ad13@gmail.com>
2024-11-13 14:47:41 +0300

    hw-3 - COWBuffer

--- homework/strings/homework_test.go
+++ homework/strings/homework_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"reflect"
+	"slices"
 	"testing"
 	"unsafe"
 
@@ -11,27 +12,47 @@ import (
 type COWBuffer struct {
 	data []byte
 	refs *int
-	// need to implement
 }
 
 func NewCOWBuffer(data []byte) COWBuffer {
-	return COWBuffer{} // need to implement
+	refs := new(int)
+	*refs = 1
+
+	return COWBuffer{
+		data: data,
+		refs: refs,
+	}
 }
 
 func (b *COWBuffer) Clone() COWBuffer {
-	return COWBuffer{} // need to implement
+	*b.refs++
+
+	return *b
 }
 
 func (b *COWBuffer) Close() {
-	// need to implement
+	*b.refs--
 }
 
 func (b *COWBuffer) Update(index int, value byte) bool {
-	return false // need to implement
+	if index < 0 || index >= len(b.data) {
+		return false
+	}
+
+	if *b.refs < 2 {
+		b.data[index] = value
+		return true
+	}
+
+	*b.refs--
+	*b = NewCOWBuffer(slices.Clone(b.data))
+	b.data[index] = value
+
+	return true
 }
 
 func (b *COWBuffer) String() string {
-	return "" // need to implement
+	return unsafe.String(unsafe.SliceData(b.data), len(b.data))
 }
 
 func TestCOWBuffer(t *testing.T) {
